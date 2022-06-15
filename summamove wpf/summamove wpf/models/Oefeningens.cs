4e7c2af1c27ac15cebc05e1311ca0438f0e0b1be
PS200183 <74544342+PS200183@users.noreply.github.com>
2022-06-15 11:59:22 +0200

    Wpf aangemaakt en models toegevoegd

---
+++ summamove wpf/summamove wpf/models/Oefeningens.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summamove_wpf.models
+{
+    public class Oefeningens
+    {
+        public int id { get; set; }
+        public string naamoefening { get; set; }
+        public string beschrijving { get; set; }
+        public string foto { get; set; }
+        
+    }
+}
