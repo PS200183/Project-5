32357d1fde054a43c4e4ba3d7a00b23634d27b07
PS200183 <74544342+PS200183@users.noreply.github.com>
2022-06-16 09:48:49 +0200

    Migration aangepast

--- SummaMove/database/migrations/2014_10_12_000000_create_users_table.php
+++ SummaMove/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('rol_id');
             $table->string('name');
-            $table->string('rol');
-            $table->string('email')->unique();  
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+
         });
     }
 
