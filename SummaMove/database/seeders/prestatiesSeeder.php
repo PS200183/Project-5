01a3e1fb8cd3e9576d5e1998c92d62d1b522d592
PS200183 <74544342+PS200183@users.noreply.github.com>
2022-06-16 11:30:00 +0200

    Seeeder en database aangepast

---
+++ SummaMove/database/seeders/prestatiesSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class prestatiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('prestaties')->insert([
+            [
+                'begintijd' => "12:00",
+                'eindtijd' => "13:00",
+                'aantal' => "1",
+                'oefening_id' => "1",
+                'user_id' => "1",
+
+            ]
+            ]);
+    }
+}
